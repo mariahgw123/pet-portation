cdc1bd84d2edac8b7657438a18d5521a873817f1
mgw008 <mariahgw123@gmail.com>
2022-11-13 10:10:38 -0700

    set tab title for browser viewing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home";
+
+//tab in browser title
+document.title = "Pet-Portation";
+//tab in browser logo
+document.logo = null;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
