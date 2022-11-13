d5b11a51d2ad9f59d929ef87e6ae0453a12c90c5
mgw008 <mariahgw123@gmail.com>
2022-11-13 10:10:06 -0700

    added homepage component

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <h1
+        className="content__title">
+            Pet-Portation
+        </h1>
+    )
+}
+
+export default Home;
