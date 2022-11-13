4dd1652cca5aadd2b25f9c8943398837c4469de0
mgw008 <mariahgw123@gmail.com>
2022-11-13 13:07:13 -0700

    bootstrap import not working. hid for now

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import "bootstrap/dist/css/boostrap.css";
+//import "bootstrap/dist/css/boostrap.css";
 import "font-awesome/css/font-awesome.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
