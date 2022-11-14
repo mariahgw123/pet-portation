41b3b20451440cd8493f893ae1b29fe1b381af05
mgw008 <mariahgw123@gmail.com>
2022-11-14 15:27:47 -0700

    Added a background video component and a content component that has a stylized header, a stylized paragraph, and a stylized button. Used useState to create a stateful hover aspect to the button to make the arrow dynamic

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
-
-//stateless component that renders
-//static "about" page data (pics, text, etc)
-//we have fun here.
+import React, {useState} from 'react';
+import { Button } from './buttonElement';
+import { 
+    Wrapper,
+    HomeBg,
+    VideoBg,
+    HomeH1,
+    HomeP,
+    HomeBtnWrapper,
+    HomeContent,
+    ArrowForward,
+    ArrowRight
+ } from './homeElements';
+ import Video from '../videos/dog_peek_vid.mp4';
 
 const Home = () => {
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent:'center',
-            alignItems: 'center',
-            height: '90vh'
-            }}>
-            <h1>Home</h1>
-        </div>
+     <>
+      <Wrapper>
+        <HomeBg>
+           <VideoBg
+            autoPlay loop muted src = {Video}
+            type='video/mp4' />
+        </HomeBg>
+        <HomeContent>
+            <HomeH1>Pet Transportation Made Easy</HomeH1>
+            <HomeP>
+                Sign up for our pet-portation services today
+                and receive $30 in credit towards your next
+                trip.
+            </HomeP>
+            <HomeBtnWrapper>
+                <Button
+                to = 'signup'
+                onMouseEnter={onHover}
+                onMouseLeave={onHover}>
+                    Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                </Button>
+            </HomeBtnWrapper>
+        </HomeContent>
+       </Wrapper>
+       </>
     )
 }
 
