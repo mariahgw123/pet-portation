a8a5c9c73fa5722f1a91521eaab6ae7a220257f3
mgw008 <mariahgw123@gmail.com>
2022-11-14 15:34:09 -0700

    added background video and stylized div wrappers. still need to figure out how to use react-scroll to be able to look through the gallery of photos

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
+import Video from '../videos/snowy-drive-vid.mp4';
+import {
+    Title,
+    Image,
+    Wrapper,
+    WrapperBg,
+    VideoBg,
+    TitleWrapper,
+    GalleryWrapper
+    } from './mediaElements';
 import bed_no_dog from '../images/bed_no_dog.png';
+import bed_with_willow from '../images/bed_with_willow.png';
 
 const Media = () => {
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent:'above',
-            alignItems: 'top',
-            height: '90vh'
-            }}>
-            <h1>Satisfied Pupstomers!</h1>
-            <img
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '690px',
-                height: '500px'
-            }}
-            src={bed_no_dog}
-            alt="bed_no_dog">
-            </img>
-        </div>
+      <>
+        <Wrapper>
+            <WrapperBg>
+            <TitleWrapper>
+                <Title>Satisfied Pupstomers!</Title>
+                <VideoBg
+                autoPlay loop muted src = {Video}
+                type = 'video/map4' />
+                </TitleWrapper>
+            </WrapperBg>
+            <GalleryWrapper>
+                <Image
+                src={bed_with_willow}
+                alt="bed_with_willow">
+                </Image>
+            </GalleryWrapper>
+        </Wrapper>
+      </>
     )
 }
 
