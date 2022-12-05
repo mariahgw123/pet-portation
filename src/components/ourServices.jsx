d4fe0ba927adccbd3898b07fe12dcdf69ded89cf
mgw008 <mariahgw123@gmail.com>
2022-12-04 23:06:11 -0700

    ok

--- src/components/ourServices.jsx
+++ src/components/ourServices.jsx
@@ -12,6 +12,7 @@ import {
     BtnWrap,
     ImgWrap,
 <<<<<<< HEAD
+<<<<<<< HEAD
     Img,
     } from './ourServicesElements';
 import { Button } from './buttonElements'; 
@@ -20,10 +21,16 @@ import { Button } from './buttonElements';
     } from './ourServicesElements';
 import { Button } from 'react-scroll'; 
 >>>>>>> e785959 (the html structure for the react scroll services page)
+=======
+    Img,
+    } from './ourServicesElements';
+import { Button } from './buttonElements'; 
+>>>>>>> 6e53f49 (passed all the styling elements and images into the services component)
 
 const OurServices = ({lightBg, id, imgStart, topLine, lightText, headline, darkText,
 description, buttonLabel, img, alt, primary, dark, dark2}) => {
     return (
+<<<<<<< HEAD
 <<<<<<< HEAD
         
      <>
@@ -48,29 +55,49 @@ description, buttonLabel, img, alt, primary, dark, dark2}) => {
                             dark2 = {dark2 ? 1 : 0}
                              >{buttonLabel}</Button>
 =======
+=======
+        
+>>>>>>> 6e53f49 (passed all the styling elements and images into the services component)
      <>
-       <InfoContainer>
+       <InfoContainer lightBg = {lightBg} id={id}>
         <InfoWrapper>
-            <InfoRow>
+            <InfoRow imgStart={imgStart}>
                 <Column1>
                     <TextWrapper>
-                        <TopLine>TopLine</TopLine>
-                        <Heading>Heading</Heading>
-                        <Subtitle>Subtitle</Subtitle>
+                        <TopLine>{topLine}</TopLine>
+                        <Heading lightText={lightText}>{headline}</Heading>
+                        <Subtitle darkText={darkText}>{description}</Subtitle>
                         <BtnWrap>
                             <Button
+<<<<<<< HEAD
                             to="home" />
 >>>>>>> e785959 (the html structure for the react scroll services page)
+=======
+                            to="home"
+                            smooth = {true}
+                            duration = {500}
+                            spy={true}
+                            exact="true"
+                            offset={-80} //offset for react scroll that has the scroll trigger at the right point
+                            primary={primary ? 1 : 0} // if primary has a value of 1 return true and v.v.
+                            dark = {dark ? 1 : 0}
+                            dark2 = {dark2 ? 1 : 0}
+                             >{buttonLabel}</Button>
+>>>>>>> 6e53f49 (passed all the styling elements and images into the services component)
                         </BtnWrap>
                     </TextWrapper>
                 </Column1>
                 <Column2>
                     <ImgWrap>
 <<<<<<< HEAD
+<<<<<<< HEAD
                     <Img src={img} alt = {alt}/>
 =======
                     <Img/>
 >>>>>>> e785959 (the html structure for the react scroll services page)
+=======
+                    <Img src={img} alt = {alt}/>
+>>>>>>> 6e53f49 (passed all the styling elements and images into the services component)
                     </ImgWrap>
                 </Column2>
             </InfoRow>
